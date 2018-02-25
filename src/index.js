fd3529188498c1d64cd2e665514f83ae97a6eec2
Mohammed Mulazada <thisismoniac@gmail.com>
2018-02-25 11:37:41 +0100

    added robohash api, added tachyon for css, now fetches robot images

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import Card from './components/Card'
+import CardList from './components/CardList'
+import registerServiceWorker from './registerServiceWorker'
+import {robots} from './robots'
+import 'tachyons'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+	<div>
+		<CardList robots={robots}/>
+	</div>
+	, document.getElementById('root'))
+registerServiceWorker()
